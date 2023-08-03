47369ec3ec113cdb265afa2905a36302cf4ea72a
Q.Tran <tranlehaiquan@gmail.com>
2023-08-03 18:59:26 +0700

    Cats: controllers, provider, dto, modules

---
+++ src/cats/cats.controller.ts
@@ -0,0 +1,48 @@
+import {
+  Controller,
+  Get,
+  Query,
+  Post,
+  Body,
+  Put,
+  Param,
+  Delete,
+} from '@nestjs/common';
+import { CreateCatDto, UpdateCatDto, ListAllEntities } from './cats.dto';
+import { CatsService } from './cats.service';
+import { Cat } from './interfaces/cat.interface';
+
+@Controller('cats')
+export class CatsController {
+  constructor(private catsService: CatsService) {}
+  @Post()
+  create(@Body() createCatDto: CreateCatDto) {
+    this.catsService.create(createCatDto);
+    return 'This action adds a new cat';
+  }
+
+  // @Get()
+  // findAll(@Query() query: ListAllEntities) {
+  //   return `This action returns all cats (limit: ${query.limit} items)`;
+  // }
+
+  @Get()
+  async findAll(): Promise<Cat[]> {
+    return this.catsService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return `This action returns a #${id} cat`;
+  }
+
+  @Put(':id')
+  update(@Param('id') id: string, @Body() updateCatDto: UpdateCatDto) {
+    return `This action updates a #${id} cat`;
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return `This action removes a #${id} cat`;
+  }
+}
