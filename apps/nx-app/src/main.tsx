c1edafc990ea9dac20de97df062e403ce0454e72
Quan Tran <tranlehaiquan@gmail.com>
2023-10-07 14:50:23 +0700

    remove web

--- apps/nx-app/src/main.tsx
+++ apps/nx-app/src/main.tsx
@@ -1,15 +1,17 @@
-import { StrictMode } from 'react';
-import * as ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
-import { Select } from 'react-components';
-import App from './app/app';
+const client = new ApolloClient({
+  uri: import.meta.env.VITE_API_URL,
+  cache: new InMemoryCache(),
+});
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement,
-);
-root.render(
-  <StrictMode>
-    <Select />
-    <App />
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </React.StrictMode>
 );
