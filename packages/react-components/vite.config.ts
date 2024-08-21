4af20f311fe34298ff22b6c5d5455194b9505897
Quan Tran <tranlehaiquan@gmail.com>
2024-08-21 18:39:27 +0700

    chore: update dependencies

--- packages/react-components/vite.config.ts
+++ packages/react-components/vite.config.ts
@@ -1,35 +1,25 @@
+/// <reference types="vitest" />
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 
-      /// <reference types="vitest" />
-      import { defineConfig } from 'vite';
-      import react from '@vitejs/plugin-react';
-      import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
-      
-      
-      export default defineConfig({
-        cacheDir: '../../node_modules/.vite/react-components',
-        
-        
-        
-    plugins: [
-      
-      react(),
-      nxViteTsPaths(),
-    ],
-    
-        
-    // Uncomment this if you are using workers. 
-    // worker: {
-    //  plugins: [ nxViteTsPaths() ],
-    // },
-        
-        
-        test: {
+export default defineConfig({
+  cacheDir: '../../node_modules/.vite/react-components',
+
+  plugins: [react(), nxViteTsPaths()],
+
+  // Uncomment this if you are using workers.
+  // worker: {
+  //  plugins: [ nxViteTsPaths() ],
+  // },
+
+  test: {
+    reporters: ['default'],
     globals: true,
     cache: {
-      dir: '../../node_modules/.vitest'
+      dir: '../../node_modules/.vitest',
     },
     environment: 'jsdom',
     include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
-    
   },
-      });
+});
